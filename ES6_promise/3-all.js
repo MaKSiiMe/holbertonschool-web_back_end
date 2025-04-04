fee0287112d66e1bcb94a574b3597234a67bb95f
MaKSiiMe <maxime.truel@gmail.com>
2025-04-04 09:54:16 +0200

    Modification des fonctions pour utiliser des promesses asynchrones et amélioration de la déstructuration des réponses.

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,9 @@
 import { uploadPhoto, createUser } from './utils.js';
 
-export default function handleProfileSignup() {
+export default async function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((resolve) => {
-      const photo = resolve[0];
-      const user = resolve[1];
+      const [photo, user] = resolve;
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
