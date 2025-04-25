8ca82dacee1bd0b69edb7e4fc452926cea8b1fae
MaKSiiMe <maxime.truel@gmail.com>
2025-04-25 11:10:59 +0200

    Ajout de docstrings pour décrire les fichiers et leur fonctionnalité dans plusieurs fichiers.

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+1. Let's execute multiple coroutines at the same time with async
+"""
 
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+4. Tasks
+"""
 
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
