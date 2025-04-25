5c9c8e2b0c1cae9c6ea131ddce1e321cbdc7907b
MaKSiiMe <maxime.truel@gmail.com>
2025-04-25 11:08:42 +0200

    Ajout de plusieurs fichiers pour implémenter des fonctions asynchrones : wait_random, wait_n, measure_time, task_wait_random et task_wait_n, ainsi qu'un README.

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """Wait for a random delay between 0 and max_delay seconds."""
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure the total time taken to execute wait_n(n, max_delay)"""
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
+    return total_time / n
