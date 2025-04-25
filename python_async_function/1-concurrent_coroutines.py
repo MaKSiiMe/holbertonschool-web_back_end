d88a515736ef2f82dd4695e00981e21d29f97a00
MaKSiiMe <maxime.truel@gmail.com>
2025-04-25 11:19:15 +0200

    Mise à jour des annotations de type pour les fonctions et amélioration de la gestion des délais dans wait_n et task_wait_n.

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,11 +4,11 @@
 """
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Wait for a random delay between 0 and max_delay seconds."""
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
-    return sorted(delays)
+    delays = [wait_random(max_delay) for _ in range(n)]
+    return [await delays for delays in asyncio.as_completed(delays)]
