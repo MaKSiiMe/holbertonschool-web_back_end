85270db9fb993974172836c9f1f3e14b871d2c31
MaKSiiMe <maxime.truel@gmail.com>
2025-04-29 16:08:32 +0200

    Ajout de fichiers pour implémenter des fonctions asynchrones : async_generator, async_comprehension et measure_runtime, ainsi qu'un README.

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+async def measure_runtime():
+    """Measures the total runtime of 4 parallel comprehensions."""
+    start_time = asyncio.get_event_loop().time()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    end_time = asyncio.get_event_loop().time()
+    return end_time - start_time
