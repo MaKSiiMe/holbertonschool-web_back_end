bff4e93fcb706b4da40f33ccffe9135d58aed96f
MaKSiiMe <maxime.truel@gmail.com>
2025-05-20 14:20:09 +0200

    Refactor le code pour améliorer la lisibilité et supprimer les importations redondantes de fs

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 const countStudents = require('./3-read_file_async');
 
 const database = process.argv[2];
@@ -13,8 +14,6 @@ const app = http.createServer((req, res) => {
     res.write('This is the list of our students\n');
     countStudents(database)
       .then(() => {
-        // eslint-disable-next-line global-require
-        const fs = require('fs');
         try {
           const data = fs.readFileSync(database, 'utf8');
           const lines = data.split('\n').filter((line) => line.trim() !== '');
@@ -22,21 +21,22 @@ const app = http.createServer((req, res) => {
           const fields = {};
           let total = 0;
           for (const student of students) {
-            // eslint-disable-next-line no-continue
-            if (student.trim() === '') continue;
             const parts = student.split(',');
-            // eslint-disable-next-line no-continue
-            if (parts.length < 4) continue;
-            const firstname = parts[0].trim();
-            const field = parts[3].trim();
-            if (!fields[field]) fields[field] = [];
-            fields[field].push(firstname);
-            total += 1;
+            if (
+              student.trim() !== ''
+              && parts.length >= 4
+            ) {
+              const firstname = parts[0].trim();
+              const field = parts[3].trim();
+              if (!fields[field]) fields[field] = [];
+              fields[field].push(firstname);
+              total += 1;
+            }
           }
           res.write(`Number of students: ${total}\n`);
-          for (const [field, names] of Object.entries(fields)) {
+          Object.entries(fields).forEach(([field, names]) => {
             res.write(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`);
-          }
+          });
           res.end();
         } catch (err) {
           res.end('Cannot load the database');
