ed90ebfd4b7f58e17f39be0cf99b4d79873b10e2
MaKSiiMe <maxime.truel@gmail.com>
2025-05-28 15:21:51 +0200

    Correction de la documentation et amélioration de la clarté des statistiques des logs Nginx dans le script log_stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
-'''Provides some stats stored in MongoDB'''
-
-
+"""programme provides some stats about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
 
 def log_stats():
-    '''Provides some stats'''
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    """The function"""
+    client = MongoClient('localhost', 27017)
     db = client.logs
     collection = db.nginx
 
     num_logs = collection.count_documents({})
-    print(f"{num_logs} logs")
-
-    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        value = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {value}")
+    status_check = collection.count_documents(
+        {
+            "method": "GET",
+            "path": "/status"
+        })
 
-    status_log = collection.count_documents(
-        {"method": "GET", "path": "/status"})
-    print(f"{status_log} status check")
+    print(f"{num_logs} logs")
+    print("Methods:")
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+    print(f"{status_check} status check")
 
 
 if __name__ == "__main__":
